b1c22cf9debad24d64dd7f19c477aac0d2e5c596
miteymouse <chenghtmark@gmail.com>
2014-05-08 10:55:36 +0800

    Adds database seed file

---
+++ server/results_db.js
@@ -0,0 +1,18 @@
+if (Results.find().count() === 0) {
+
+    // Results.insert({
+    //   Race1: {
+    //     win: {winning_combination: ["4",], dividend: "599.5"}
+
+
+    //   }
+    // });
+
+    Results.insert({
+         dividend: "599.5"
+
+        })
+
+}
+
+
