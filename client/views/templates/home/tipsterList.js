f83ea148a5092301d3968a19835c29454c0bde67
Screen Goose <sarrieschaz@hotmail.com>
2014-05-11 20:13:46 +0800

    Added dynamic loading from the home page

---
+++ client/views/templates/home/tipsterList.js
@@ -0,0 +1,17 @@
+Template.home.helpers({
+  tipster: function() {
+    var data = [];
+
+    Meteor.users.find().forEach( function(obj) {
+      p1 = {
+        tipsterName: obj.profile.name,
+        score: obj.profile.score,
+        _id: obj._id
+      };
+
+      data.push(p1);
+    });
+
+    return data;
+  }
+});
