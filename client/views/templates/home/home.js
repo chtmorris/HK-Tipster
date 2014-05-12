d9030026db59f2c9367f9074ee8c80e5bfd6e2b0
Screen Goose <sarrieschaz@hotmail.com>
2014-05-12 19:24:44 +0800

    Added new route and edit page

--- client/views/templates/home/home.js
+++ client/views/templates/home/home.js
@@ -1,10 +1,7 @@
-Template.home.helpers({
-  username: function() {
-    return "Trainer";
-  },
-
-  id: function() {
-    return "QSHY3LbjmRCuvuRXJ";
-  }
-
+Template.home.events({                                                                                    // 13
+  'click #login-buttons-logout': function() {                                                                    // 14
+    Meteor.logout(function() {                                                                                 // 15
+      loginButtonsSession.closeDropdown();                                                                   // 16
+    });                                                                                                        // 17
+  }                                                                                                              // 18
 });
--- client/views/templates/profile/tipster_stats.js
+++ client/views/templates/profile/tipster_stats.js
@@ -1,12 +1,14 @@
 Template.tipster_tips.helpers({
 
-  horse1_race1: function() {
+    getHorse: function(race, horse) {
     var url = window.location.pathname;
     var id = url.substring(url.lastIndexOf('/') + 1);
     var user = Meteor.users.find( { _id: id } );
     if (user.count() == 0)
       return '';
     else
-      return user.fetch()[0].profile.race.raceDate.race1.horse_1;
+      return user.fetch()[0].profile.race.raceDate[race][horse];
   }
+
 });
+
