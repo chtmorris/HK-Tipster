2ac08f16f51fe380e59f23fbdab1e31c5c683e34
Screen Goose <sarrieschaz@hotmail.com>
2014-05-13 09:34:25 +0800

    New routing

--- client/views/templates/tipster_profile/winnings_followers_text.js
+++ client/views/templates/tipster_profile/winnings_followers_text.js
@@ -1,8 +1,6 @@
 Template.winnings_followers_text.helpers({
 
   score: function() {
-    // console.log('loggin in: ' + Meteor.loggingIn());
-    // console.log('user: ' + Meteor.user());
     if (Meteor.user())
       return Meteor.user().profile.score;
     else
