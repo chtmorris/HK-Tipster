415479c863b0aa11f9e3b881a7b9aea42696b8d3
miteymouse <chenghtmark@gmail.com>
2014-05-14 10:53:20 +0800

    Raceday pulls horses from database

--- client/views/templates/raceday/raceday.js
+++ client/views/templates/raceday/raceday.js
@@ -13,43 +13,34 @@ Template.raceday.helpers ({
 
     }
 
-
     return followee_ids;
-    // return ["AgsNmjvTNv3WoroCi"];
-
-
-    // if (Meteor.user()) {
-    //   for (id in Meteor.user().profile.race.raceDate.followee) {
-    //     followee_ids.push(Meteor.user().profile.race.raceDate.followee[id]);
-    //   }
-    // } else {
-
-    // console.log(followee_ids);
-    // console.log("no user");
-    // }
-
-
 
     }
 
-    // myParam: function() {
 
-    //   return _.extend({paramName: "123"},this);
-    // }
 
 });
 
-Template.followee_picks.helpers ({
+Template.raceday.helpers ({
 
   getHorse: function(race, horse, id) {
-
     var user = Meteor.users.find( { _id: id } );
     if (user.count() == 0)
       return '';
     else
       return user.fetch()[0].profile.race.raceDate[race][horse];
+  },
+
+  key_value: function(context, options) {
+     var result = [];
+      _.each(context, function(value, key, list){
+      result.push({race:key+1, followee:value});
+      })
+    return result;
   }
+
 });
+
 
 
 
